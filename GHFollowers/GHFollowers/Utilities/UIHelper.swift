883875a45f09b7289754040ae0afef4d1c63376f
jas2996 <jaskiran.lamba@gmail.com>
2022-04-17 21:31:56 -0400

    add UIHelper

---
+++ GHFollowers/GHFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,26 @@
+//
+//  UIHelper.swift
+//  GHFollowers
+//
+//  Created by Jas Lamba on 2022-04-17.
+//
+
+import UIKit
+
+struct UIHelper {
+    static func createThreeColumnFolowLayout(in view: UIView) -> UICollectionViewLayout {
+        //this is how the collection view will look like
+        let width = view.bounds.width
+        let padding: CGFloat = 12 //left and right side of the screen
+        let minimumItemSpacing: CGFloat = 10 //between each cell
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        //height includes the avatar image and also the username label
+        
+        return flowLayout
+    }
+}
