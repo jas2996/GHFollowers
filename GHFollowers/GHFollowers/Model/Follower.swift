b75828ef0cef03cdfb515def7a0622def23c20af
jas2996 <jaskiran.lamba@gmail.com>
2022-04-17 21:12:06 -0400

    add hashable protocol to data model

--- GHFollowers/GHFollowers/Model/Follower.swift
+++ GHFollowers/GHFollowers/Model/Follower.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
-struct Follower: Codable {
+struct Follower: Codable, Hashable {
     // when you conform to the codable protocol you get both encodable and decodable
     var login: String
     var avatarUrl: String
+    
+//    MARK: - Only if you want one of the item to be hashale
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(login)
+//    }
 }
