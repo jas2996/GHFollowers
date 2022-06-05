057ee5705874b0a0bec12f53fbe529cc51416b16
Jas Lamba <jaskiran.lamba@gmail.com>
2022-06-05 17:39:56 -0400

    feat: add constants file

---
+++ GHFollowers/GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Jas Lamba on 2022-06-05.
+//
+
+import Foundation
+
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
