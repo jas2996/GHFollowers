b669b6c46834ddc138a6631406af41810f8bfcd7
Jas Lamba <jaskiran.lamba@gmail.com>
2022-09-29 14:12:28 -0400

    refactor: move loading view out of extension

--- GHFollowers/GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/GHFollowers/Screens/FollowerListVC.swift
@@ -11,7 +11,7 @@ protocol FollowerListVCDelegate: class {
     func didRequestFollowers(for username: String)
 }
 
-class FollowerListVC: UIViewController {
+class FollowerListVC: GFDataLoadingVC {
     
     //enums are hashable by default
     enum Section {
