f46a92b5b403f50866aaf556134be242bb6738c0
Jas Lamba <jaskiran.lamba@gmail.com>
2022-10-05 19:21:36 -0400

    refactor: remove favourite from array only on success

--- GHFollowers/GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/GHFollowers/Screens/FavoritesListVC.swift
@@ -85,13 +85,14 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
-        let favourite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .left)
         
-        PersistenceManager.updateWith(favourite: favourite, actionType: .remove) { [weak self] error in
+        PersistenceManager.updateWith(favourite: favorites[indexPath.row], actionType: .remove) { [weak self] error in
             guard let self = self else { return }
-            guard let error = error else { return }
+            guard let error = error else {
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .left)
+                return
+            }
             
             self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
         }
